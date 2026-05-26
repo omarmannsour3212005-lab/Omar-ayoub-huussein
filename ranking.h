a8312a035604684c9745d7fcf9fa9b0b8742dfe7
housseinmcheik814-eng <housseinmcheik814@gmail.com>
2026-05-26 15:08:55 +0200

    Update ranking.h

--- ranking.h
+++ ranking.h
@@ -1,1 +1,13 @@
+#ifndef RANKING_H
+#define RANKING_H
 
+// Calculate player points
+int calculatePlayerPoints();
+
+// Show tournament ranking
+void showTournamentRanking();
+
+// Show general ranking
+void showGeneralRanking();
+
+#endif
