fb3f359c621423c25f25b39f3b9ecb16774785fa
housseinmcheik814-eng <housseinmcheik814@gmail.com>
2026-05-26 16:55:00 +0200

    houssein

--- ranking.c
+++ ranking.c
@@ -1,40 +1,89 @@
 #include <stdio.h>
-
+#include "models.h"
 #include "ranking.h"
 
+// ==================== CALCULATE PLAYER POINTS ====================
 
-// ==================== PLAYER POINTS ====================
-
-int calculatePlayerPoints() {
-
+int calculatePlayerPoints(Match match, int playerId)
+{
     int points = 0;
 
-    // TODO:
-    // Add score calculations here
+    // PLAYER 1
+    if(playerId == match.player1Id)
+    {
+        // Vittoria
+        if(match.winnerId == playerId)
+        {
+            points += 10;
+        }
+
+        // Ace
+        points += match.ace1 * 2;
+
+        // Break
+        points += match.break1;
+
+        // Doppi falli
+        points -= match.doppiFalli1;
+    }
+
+    // PLAYER 2
+    else if(playerId == match.player2Id)
+    {
+        // Vittoria
+        if(match.winnerId == playerId)
+        {
+            points += 10;
+        }
+
+        // Ace
+        points += match.ace2 * 2;
+
+        // Break
+        points += match.break2;
+
+        // Doppi falli
+        points -= match.doppiFalli2;
+    }
 
     return points;
 }
 
+// ==================== SHOW TOURNAMENT RANKING ====================
 
-// ==================== TOURNAMENT RANKING ====================
+void showTournamentRanking()
+{
+    FILE *fp;
 
-void showTournamentRanking() {
+    Match match;
 
-    printf("\n=== TOURNAMENT RANKING ===\n");
+    fp = fopen("matches.dat", "rb");
 
-    // TODO:
-    // Ranking logic
+    if(fp == NULL)
+    {
+        printf("Nessun match trovato!\n");
+        return;
+    }
 
-}
+    printf("\n===== TOURNAMENT RANKING =====\n");
 
+    while(fread(&match, sizeof(Match), 1, fp))
+    {
+        int p1Points;
+        int p2Points;
 
-// ==================== GENERAL RANKING ====================
+        p1Points = calculatePlayerPoints(match, match.player1Id);
 
-void showGeneralRanking() {
+        p2Points = calculatePlayerPoints(match, match.player2Id);
 
-    printf("\n=== GENERAL RANKING ===\n");
+        printf("\nPlayer %d -> %d punti",
+               match.player1Id,
+               p1Points);
 
-    // TODO:
-    // Global ranking logic
+        printf("\nPlayer %d -> %d punti\n",
+               match.player2Id,
+               p2Points);
+    }
 
+    fclose(fp);
 }
