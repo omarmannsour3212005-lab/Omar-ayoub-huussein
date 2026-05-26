e321ddb83857bd212530e8c31741a193c3b9faf9
housseinmcheik814-eng <housseinmcheik814@gmail.com>
2026-05-26 15:07:19 +0200

    houssein

--- coach.h
+++ coach.h
@@ -1,1 +1,15 @@
+#ifndef COACH_H
+#define COACH_H
 
+#include "models.h"
+
+// Register new coach
+void registerCoach();
+
+// View all coaches
+void viewCoaches();
+
+// Check if nickname already exists
+int coachExists(char nickname[]);
+
+#endif
