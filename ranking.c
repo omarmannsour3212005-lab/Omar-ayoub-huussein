a4341333a8874d7cc490f1a74c5d4188085b820b
housseinmcheik814-eng <housseinmcheik814@gmail.com>
2026-05-26 15:11:08 +0200

    houssein

--- ranking.c
+++ ranking.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+#include "ranking.h"
+
+
+// ==================== PLAYER POINTS ====================
+
+int calculatePlayerPoints() {
+
+    int points = 0;
+
+    // TODO:
+    // Add score calculations here
+
+    return points;
+}
+
+
+// ==================== TOURNAMENT RANKING ====================
+
+void showTournamentRanking() {
+
+    printf("\n=== TOURNAMENT RANKING ===\n");
+
+    // TODO:
+    // Ranking logic
+
+}
+
+
+// ==================== GENERAL RANKING ====================
+
+void showGeneralRanking() {
+
+    printf("\n=== GENERAL RANKING ===\n");
+
+    // TODO:
+    // Global ranking logic
+
+}
