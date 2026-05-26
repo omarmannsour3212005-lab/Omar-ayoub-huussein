6daaa86ec191da9f37aba6ca40b9499559494614
Ayo <hajjiayoub3242@gmail.com>
2026-05-26 15:07:31 +0200

    Ayoub

--- tornei.h
+++ tornei.h
@@ -1,1 +1,17 @@
+#ifndef TORNEI_H
+#define TORNEI_H
 
+#include "models.h"
+
+// ============================
+// FUNCTIONS
+// ============================
+
+void addTorneo();
+void viewTornei();
+void searchTorneo();
+void deleteTorneo();
+
+int torneoExists(int id);
+
+#endif
