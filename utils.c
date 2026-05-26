78abd36b09ac9816db323b2a0beb04fac2b03061
housseinmcheik814-eng <housseinmcheik814@gmail.com>
2026-05-26 14:59:47 +0200

    HOUSSE

--- utils.c
+++ utils.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include "utils.h"
 
+void clearInputBuffer() {
+
+    int c;
+
+    while ((c = getchar()) != '\n' && c != EOF);
+
+}
+
+void pauseProgram() {
+
+    printf("\nPress Enter to continue...");
+    getchar();
+
+}
