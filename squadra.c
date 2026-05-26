28ce7a48dd72cdcb4558e18dcc8300d4e34691c0
omarmannsour3212005-lab <omarmannsour3212005@gmail.com>
2026-05-26 15:15:36 +0200

    Update squadra.c

--- squadra.c
+++ squadra.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "squadra,.h"
 
+void createTeam() {
+  printf("\nCreazione squdara in sviluppo...\n");
+}
+
+void modifyTeam() {
+  printf("\nModifica squadra in sviluppo...\n");
+}
+
+void displayTeam() { 
+  printf("\nVisualizzaione squadra in sviluppo...\n");
+}
