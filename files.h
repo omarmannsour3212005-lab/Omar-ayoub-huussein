3e214a74f7f5bfd822ea56d28a2e8148ead5d93f
Ayo <hajjiayoub3242@gmail.com>
2026-05-26 15:10:44 +0200

    Ayoub

--- files.h
+++ files.h
@@ -1,1 +1,18 @@
+#ifndef FILES_H
+#define FILES_H
 
+#include "tennisti.h"
+
+void salvaTennista(Tennista t);
+
+void leggiTennisti();
+
+int trovaTennistaByID(int id, Tennista *t);
+
+void aggiornaTennista(Tennista t);
+
+void eliminaTennistaFile(int id);
+
+int esisteID(int id);
+
+#endif
